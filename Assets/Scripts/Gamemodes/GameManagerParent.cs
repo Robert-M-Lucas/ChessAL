8f2cfd307d97a93748f836113d01a8b7a8016a10
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-10 23:25:07 +0100

    Change team size structure

--- Assets/Scripts/Gamemodes/GameManagerParent.cs
+++ Assets/Scripts/Gamemodes/GameManagerParent.cs
@@ -13,13 +13,6 @@ public abstract class GameManagerParent
     /// <summary>
     /// 
     /// </summary>
-    /// <returns>Minimum players to start game</returns>
-    public abstract int GetMinPlayers();
-    public abstract int GetMaxPlayers();
-
-    /// <summary>
-    /// 
-    /// </summary>
     /// <returns>{ Team1Size, Team2Size, Team3Size, ... }</returns>
-    public abstract int[] TeamSizes();
+    public abstract TeamSize[] TeamSizes();
 }
