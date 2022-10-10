1c01962812404763aab811bcb47a3927236bff1c
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-10 23:25:26 +0100

    Added summaries for ArrayExtensions

--- Assets/Scripts/Networking/ArrayExtentions.cs
+++ Assets/Scripts/Networking/ArrayExtentions.cs
@@ -2,44 +2,61 @@ using System;
 using System.Collections.Generic;
 using System.Collections;
 
+/// <summary>
+/// Provides additional functions for handling byte[]
+/// </summary>
 public static class ArrayExtensions
 {
-    public static byte[] Merge(byte[] BigArr, byte[] SmallArr, int index = 0)
+    /// <summary>
+    /// Overwrites part of an array with another
+    /// </summary>
+    /// <param name="bigArr"></param>
+    /// <param name="smallArr"></param>
+    /// <param name="index">Overwrite start</param>
+    /// <returns></returns>
+    public static byte[] Merge(byte[] bigArr, byte[] smallArr, int index = 0)
     {
-        foreach (byte item in SmallArr)
+        foreach (byte item in smallArr)
         {
-            BigArr[index] = item;
+            bigArr[index] = item;
             index++;
         }
 
-        return BigArr;
+        return bigArr;
     }
 
-    public static byte[] Slice(byte[] Arr, int start, int end)
+    /// <summary>
+    /// Gets a smaller array out of a larger one
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="start">(inclusive)</param>
+    /// <param name="end">(exclusive)</param>
+    /// <returns></returns>
+    public static byte[] Slice(byte[] arr, int start, int end)
     {
         byte[] to_return = new byte[end - start];
         var index = 0;
         for (int i = start; i < end; i++)
         {
-            to_return[index] = Arr[i];
+            to_return[index] = arr[i];
             index++;
         }
         return to_return;
     }
 
-    public static Tuple<byte[], int> ClearEmpty(byte[] Arr)
+    public static Tuple<byte[], int> ClearEmpty(byte[] arr)
     {
         int index = 0;
-        for (int i = 0; i < Arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
         {
-            if (Arr[i] != 0)
+            if (arr[i] != 0)
             {
                 index = i;
                 break;
             }
         }
         return new Tuple<byte[], int>(
-            Merge(new byte[Arr.Length], Slice(Arr, index, Arr.Length)),
+            Merge(new byte[arr.Length], Slice(arr, index, arr.Length)),
             index
         );
     }
