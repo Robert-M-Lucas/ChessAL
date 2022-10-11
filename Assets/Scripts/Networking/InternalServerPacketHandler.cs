bda1b55227e0e2c5dc763044c431794184cd99e9
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-11 16:49:10 +0100

    Created InternalServerPacketHandler

---
+++ Assets/Scripts/Networking/InternalServerPacketHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InternalServerPacketHandler
+{
+    private Server server;
+
+    public Dictionary<int, Action<Packet>> UIDtoAction { get; }
+
+    public InternalServerPacketHandler(Server server)
+    {
+        this.server = server;
+        
+        UIDtoAction = new Dictionary<int, Action<Packet>> {
+            { 4, PingRespond },
+            { 7, RemoveClient }
+        };
+    }
+
+    public bool TryHandlePacket(Packet packet)
+    {
+        if (!UIDtoAction.ContainsKey(packet.UID)) return false;
+
+        UIDtoAction[packet.UID](packet);
+        return true;
+    }
+
+    public void PingRespond(Packet packet)
+    {
+        server.SendMessage(packet.From, ServerPingPacket.Build());
+    }
+
+    public void RemoveClient(Packet packet)
+    {
+        server.TryRemovePlayer(packet.From, "Disconnected");
+    }
+}
