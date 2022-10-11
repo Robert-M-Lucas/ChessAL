0be56a4f67a656c1ce02be1deb007ed38d4695e0
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-11 16:36:31 +0100

    Created ServerPlayerData

---
+++ Assets/Scripts/Networking/ServerPlayerData.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Data server holds about player
+/// </summary>
+public class ServerPlayerData
+{
+    public int PlayerID;
+    public string Name;
+
+    public int Team;
+
+    /// <summary>
+    /// E.g. Player *1* in team x
+    /// </summary>
+    public int PlayerInTeam;
+
+    public ServerPlayerData(Socket handler, int playerID, string name, int team, int playerInTeam)
+    {
+        Handler = handler;
+        PlayerID = playerID;
+        Name = name;
+        Team = team;
+        PlayerInTeam = playerInTeam;
+    }
+
+    /// <summary>
+    /// Returns a unique string that represents the player
+    /// </summary>
+    /// <returns></returns>
+    public string GetUniqueString()
+    {
+        return "[" + PlayerID + "] " + Name + "";
+    }
+
+    // Networking
+
+    public Socket Handler;
+
+    public byte[] Buffer = new byte[1024];
+    public byte[] LongBuffer = new byte[1024];
+    public int CurrentPacketLength = -1;
+    public int LongBufferSize = 0;
+    public StringBuilder SB = new StringBuilder();
+
+    public void Reset()
+    {
+        Buffer = new byte[1024];
+        SB = new StringBuilder();
+    }
+
+    /// <summary>
+    /// Shuts down the connection between the client and the server
+    /// </summary>
+    public void ShutdownSocket()
+    {
+        Handler.Shutdown(SocketShutdown.Both);
+        Handler.Close();
+    }
+}
