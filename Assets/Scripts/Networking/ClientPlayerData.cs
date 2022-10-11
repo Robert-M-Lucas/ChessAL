3c25404b56f0bbcf9038725c533e900287945eb2
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-11 20:01:45 +0100

    Added ClientPlayerData

--- Assets/Scripts/Networking/ClientPlayerData.cs
+++ Assets/Scripts/Networking/ClientPlayerData.cs
@@ -3,4 +3,22 @@
 /// </summary>
 public class ClientPlayerData
 {
+    public int PlayerID;
+    public string Name;
+
+    public int Team;
+
+    /// <summary>
+    /// E.g. Player *1* in team x
+    /// </summary>
+    public int PlayerInTeam;
+
+    public ClientPlayerData(int playerID, string name, int team, int playerInTeam)
+    {
+        PlayerID = playerID;
+        Name = name;
+        Team = team;
+        PlayerInTeam = playerInTeam;
+    }
+
 }
