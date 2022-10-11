6f98d68ac08e3373ea2e20371ee9574c8a7b23ac
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-11 16:36:21 +0100

    Created ClientPlayerData

---
+++ Assets/Scripts/Networking/ClientPlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Data client holds about other players
+/// </summary>
+public class ClientPlayerData
+{
+    
+}
