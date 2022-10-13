db6b2c3a78a8943da3bd3592089a6390435ecfb2
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-13 20:22:58 +0100

    Added packets for syncing gamemode

---
+++ Assets/Scripts/Networking/Packets/200_GamemodeDataRequestPacket.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class GamemodeDataRequestPacket {
+    public const int UID = 200;
+    public GamemodeDataRequestPacket(Packet packet){
+    }
+
+    public static byte[] Build() {
+            List<byte[]> contents = new List<byte[]>();
+            return PacketBuilder.Build(UID, contents);
+    }
+}
---
+++ Assets/Scripts/Networking/Packets/201_GamemodeDataPacket.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class GamemodeDataPacket {
+    public const int UID = 201;
+    public int Gamemode;
+    public byte[] SaveData;
+    public GamemodeDataPacket(Packet packet){
+        Gamemode = BitConverter.ToInt32(packet.Contents[0]);
+        SaveData = (packet.Contents[1]);
+    }
+
+    public static byte[] Build(int _Gamemode, byte[] _SaveData) {
+            List<byte[]> contents = new List<byte[]>();
+            contents.Add(BitConverter.GetBytes(_Gamemode));
+            contents.Add(_SaveData);
+            return PacketBuilder.Build(UID, contents);
+    }
+}
