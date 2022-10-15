0dfade40169c0f163deb168016d1b38ed32ca0b5
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-15 20:50:38 +0100

    Fixed nullability issues Removed unused methods Added summarues Added support for game starting Removed unnecessary data passing Added HostSetTeam support

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 /// </summary>
 public class NetworkManager : MonoBehaviour
 {
-    private ChessManager chessManager;
+    private ChessManager chessManager = default!;
 
     private Server? server = null;
     private Client? client = null;
@@ -21,15 +21,6 @@ public class NetworkManager : MonoBehaviour
     {
         DontDestroyOnLoad(this);
         chessManager = FindObjectOfType<ChessManager>();
-    }
-
-    private void Start()
-    {
-
-    }
-
-    private void Update()
-    {
     }
 
     private void OnApplicationQuit()
@@ -48,7 +39,13 @@ public class NetworkManager : MonoBehaviour
 
     public void OnLocalMove(MoveData moveData) => client?.OnLocalMove(moveData);
 
+    /// <summary>
+    /// Gets the player list from the client
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException"></exception>
     public ConcurrentDictionary<int, ClientPlayerData> GetPlayerList() => client?.PlayerData ?? throw new NullReferenceException();
+    public int GetLocalPlayerID() => client!.PlayerID;
 
     /// <summary>
     /// Hosts a game
@@ -60,25 +57,31 @@ public class NetworkManager : MonoBehaviour
         server = new Server(gameData, settings.Password);
         server.Start();
 
-        client = new Client("127.0.0.1", settings.Password, settings.PlayerName, this);
+        client = new Client("127.0.0.1", settings.Password, settings.PlayerName, this, OnHostSuccessOrFail);
         client.Connect();
     }
 
-    public void HostStartGame()
+    /// <summary>
+    /// Starts a game
+    /// </summary>
+    /// <returns>Null if successful or a string error</returns>
+    public string? HostStartGame()
     {
-        server?.StartGame();
+        return server?.StartGame();
     }
 
+    /// <summary>
+    /// Joins a game
+    /// </summary>
+    /// <param name="settings"></param>
     public void Join(JoinSettings settings)
     {
-        client = new Client(settings.IP, settings.Password, settings.PlayerName, this);
+        client = new Client(settings.IP, settings.Password, settings.PlayerName, this, OnJoinSuccessOrFail);
         client.Connect();
     }
 
-    
-
     #region Client Callbacks
-    public void OnHostSuccessOrFail(string? status, AbstractGameManagerData? gameMode)
+    public void OnHostSuccessOrFail(string? status)
     {
         if (status is not null)
         {
@@ -91,7 +94,7 @@ public class NetworkManager : MonoBehaviour
 
         chessManager.HostSucceed();
     }
-    public void OnJoinSuccessOrFail(string? status, AbstractGameManagerData? gameMode)
+    public void OnJoinSuccessOrFail(string? status)
     {
         if (status is not null)
         {
@@ -109,7 +112,7 @@ public class NetworkManager : MonoBehaviour
     public void OnGamemodeRecieve(int gameMode, byte[] saveData) => chessManager.GameDataRecived(gameMode, saveData);
     public void OnGameStart() => chessManager.OnGameStart();
     public void OnForeignMove(MoveData moveData) => chessManager.OnForeignMove(moveData);
-    public void OnTurn() => chessManager.OnTurn();
     private void OnPing(int ping) => Debug.Log($"Ping {ping}ms");
+    public void HostSetTeam(int playerID, int team, int playerInTeam) => server?.SetTeam(playerID, team, playerInTeam);
     #endregion
 }
