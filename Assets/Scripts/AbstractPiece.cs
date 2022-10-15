fd5c78ccb8a322e1fe0a359b0839e97a6cf77167
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-15 20:55:05 +0100

    Added core functionality

--- Assets/Scripts/AbstractPiece.cs
+++ Assets/Scripts/AbstractPiece.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 /// <summary>
 /// A game piece
 /// </summary>
-public class AbstractPiece 
+public abstract class AbstractPiece 
 {
+    public V2 Position;
+    /// <summary>
+    /// ID corresponding to sprites in the VisualManager
+    /// </summary>
+    public int AppearanceID;
+
+    public AbstractPiece(V2 position)
+    {
+        Position = position;
+    }
+
     public virtual List<Move> GetMoves() { return new List<Move>(); }
+
+    public abstract int GetUID();
 }
