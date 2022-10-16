6916fb3595afb6935489c58960e17a3f419cf81a
Robert-App-Dev <robert01.appdev@gmail.com>
2022-10-16 20:53:11 +0100

    Added support for registering clicks on pieces

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,6 +9,13 @@ public class InputManager : MonoBehaviour
 {
     private List<Move> possibleMoves = new List<Move>();
 
+    private VisualManager visualManager;
+
+    public void Start()
+    {
+        visualManager = FindObjectOfType<VisualManager>();
+    }
+
     /// <summary>
     /// Updates the list of moves the player can make
     /// </summary>
@@ -16,5 +23,15 @@ public class InputManager : MonoBehaviour
     public void SetPossibleMoves(List<Move> possibleMoves)
     {
         this.possibleMoves = possibleMoves;
+    }
+
+    public void OnPieceClick(V2 position)
+    {
+        visualManager.ShowMoves(position);
+    }
+
+    private void Update()
+    {
+
     }
 }
