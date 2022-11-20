42f89120c74cef5ae25e84edf8cb60976122bcbc
Robert-App-Dev <robert01.appdev@gmail.com>
2022-11-20 18:47:18 +0000

    Added support for client disconnects

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -36,11 +36,8 @@ public class NetworkManager : MonoBehaviour
     /// </summary>
     /// <returns></returns>
     /// <exception cref="NullReferenceException"></exception>
-    public ConcurrentDictionary<int, ClientPlayerData> GetPlayerList()
-    {
-        Debug.LogWarning(GetHashCode());
-        return client!.PlayerData;
-    }
+    public ConcurrentDictionary<int, ClientPlayerData> GetPlayerList() => client!.PlayerData;
+
     public int GetLocalPlayerID()
     {
         Debug.LogWarning(GetHashCode());
